d9bc9077289ac1783e663c19496342a99a83ede2
Le Coursier <garatakpara@gmail.com>
2025-09-19 03:08:46 +0200

    Update main.tf
    terraform {
      required_providers {
        aws = {
          source  = "hashicorp/aws"
          version = "~> 5.0"
        }
      }
    }
    
    provider "aws" {
      region = "eu-west-3"  # Paris
    }
    
    # Module pour créer un serveur de base de données
    module "database" {
      source = "../modules/database"
    
      environment_name = "staging"
      db_instance_type = "db.t4g.micro"
    }

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -1,1 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = "eu-west-3"  # Paris
+}
+
+# Module pour créer un serveur de base de données
+module "database" {
+  source = "../modules/database"
+
+  environment_name = "staging"
+  db_instance_type = "db.t4g.micro"
+}
